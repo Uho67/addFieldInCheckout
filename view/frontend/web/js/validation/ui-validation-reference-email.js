67f3718fca9ac29e73831f1b50bf1f79f401687c
vagrant <vagrant@devbox.vaimo.test>
2020-01-31 07:33:15 +0100

    30.01;

--- view/frontend/web/js/validation/ui-validation-reference-email.js
+++ view/frontend/web/js/validation/ui-validation-reference-email.js
@@ -4,36 +4,37 @@
  */
 
 define([
-           'jquery',
+           'jquery'
        ], function ($) {
     'use strict';
 
     return function (validator) {
+
         validator.addRule(
             'reference-email',
-            function (value, params, additionalParams) {
-                var data = {};
-                data.email = value;
-                // var customersEmails = ['asd0@gmail.com', 'asd1@gmail.com', 'asd2@gmail.com'];
-                // return $.inArray(value, customersEmails) !== -1;
-                $.ajax({
-                           url: '/ClemondoMarkGoods/referencecustomer/checkemail',
-                           type: 'post',
-                           data: data,
-                           success: function (response) {
-                              console.log(response);
-                           },
-                           error: function(response) {
-                               console.log(response)
-                           }
-                       });
-
-                // $.post('/example.php', serializedData, function(response) {
-                //     console.log("Response: "+response);
-                // });
+            function (value) {
+                if(value.length>0) {
+                    var result = false;
+                    $('body').trigger('processStart');
+                    $.ajax({
+                               url: 'http://devbox.vaimo.test/clemondo/ClemondoMarkGoods/referencecustomer/checkemail',
+                               type: 'post',
+                               data: {
+                                   email: value
+                               },
+                               async: false,
+                               success: function(response) {
+                                   result = response;
+                                   $('body').trigger('processStop');
+                               }
+                           });
+                    return result;
+                }
+                return true;
             },
             $.mage.__("Please specify a valid reference email")
         );
+
         return validator;
     }
 });
